f5140cc009b14632906bb8cf411feb9781756006
borhanrahi <borhanrahi111@gmail.com>
2023-03-01 19:44:25 +0600

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,22 @@ import ContentTwo from '../components/ContentTwo'
 import Faq from './../components/Faq';
 import CallTwoAct from './../components/CallTwoAct';
 import Footer from './../components/Footer';
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
+         <Head>
+        <title>
+        Blissful Balance
+        </title>
+        <meta
+          name="description"
+          content="Blissful Balance The Brand New For You!"
+          key="desc"
+        />
+      </Head>
+
       <Hero />
       <MainFeature/> 
       <ContentOne/>
